28f41cc96d79125bca0563287b5443d80b393b91
andy <andy.lv.vip@hotmail.com>
2019-01-17 00:13:33 +0800

    init

---
+++ p_corporate/src/main/java/uca/platform/corporate/PCorporateApplication.java
@@ -0,0 +1,35 @@
+package uca.platform.corporate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RefreshScope
+@EnableEurekaClient
+@EnableCircuitBreaker
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class PCorporateApplication {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(OAuth2ProtectedResourceDetails details,
+                                     OAuth2ClientContext oauth2ClientContext) {
+        return new OAuth2RestTemplate(details, oauth2ClientContext);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(PCorporateApplication.class, args);
+    }
+}
