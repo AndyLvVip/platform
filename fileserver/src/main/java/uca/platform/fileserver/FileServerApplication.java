29c40574eafd8c42d8c01598b490c05207b9ec21
andy.lv <lvhs@ucacc.com>
2019-01-31 22:13:29 +0800

    complete the file upload...

--- fileserver/src/main/java/uca/platform/fileserver/FileServerApplication.java
+++ fileserver/src/main/java/uca/platform/fileserver/FileServerApplication.java
@@ -4,10 +4,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jooq.conf.Settings;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import uca.platform.common.jooq.ObjectFactoryUtils;
 import uca.platform.factory.StdObjectFactory;
@@ -41,6 +44,21 @@ public class FileServerApplication {
     @Bean
     public Settings settings() {
         return ObjectFactoryUtils.settings();
+    }
+
+    @Bean
+    public UserInfoTokenServices userInfoTokenServices(ResourceServerProperties sso
+                                                       , OAuth2RestOperations restTemplate
+//                                                       , PrincipalExtractor principalExtractor
+    ) {
+        UserInfoTokenServices services = new UserInfoTokenServices(
+                sso.getUserInfoUri(), sso.getClientId());
+        services.setRestTemplate(restTemplate);
+        services.setTokenType(sso.getTokenType());
+//        if (principalExtractor != null) {
+//            services.setPrincipalExtractor(principalExtractor);
+//        }
+        return services;
     }
 
 }
--- m_corporate/src/main/resources/schema.sql
+++ m_corporate/src/main/resources/schema.sql
@@ -6,10 +6,28 @@ create table corporate
   enabled   boolean      not null,
   status    tinyint      not null,
   type      tinyint      not null,
-  createdBy varchar(30)  not null,
-  createdOn datetime(3)  not null,
+  created_by varchar(30)  not null,
+  created_on datetime(3)  not null,
   version   int          not null,
-  updatedBy varchar(30)  not null,
-  updatedOn datetime(3)  not null
+  updated_by varchar(30)  not null,
+  updated_on datetime(3)  not null
 ) engine = InnoDB,
   charset = utf8mb4;
+
+drop table if exists user;
+create table user (
+  id char(36) not null primary key ,
+  username varchar(30) not null,
+  email varchar(255) null,
+  phone char(11) not null,
+  name varchar(20) not null,
+  member_of char(36) not null,
+  version int not null,
+  created_by varchar(30) not null,
+  created_on datetime(3) not null,
+  updated_by varchar(30) not null,
+  updated_on datetime(3) not null,
+  index idx_member_of (member_of)
+) engine = InnoDB,
+  charset = utf8mb4;
+
